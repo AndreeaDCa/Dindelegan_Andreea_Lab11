b3d78757537f755529d00fbac0e36bc89c3644db
AndreeaDCa <andreea.dindelegan@stud.ubbcluj.ro>
2023-12-11 19:42:50 +0200

    Add project files.

---
+++ Dindelegan_Andreea_Lab11/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Dindelegan_Andreea_Lab11.Data;
+
+namespace Dindelegan_Andreea_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Dindelegan_Andreea_Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Dindelegan_Andreea_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
